25dc83b82854c05fe68ad9e09b2e646558ff7301
guzhenfu <gzfgeh@sina.com>
2019-04-07 23:56:53 +0800

    update file

--- expressH5/js/pay.js
+++ expressH5/js/pay.js
@@ -6,82 +6,86 @@
 
 function  payModal(money)
 {
-if(monthPay){
-	$("body").append('<div class="modal-mask col" id="payModal">'+
-	    '<div style="flex: 1;"></div>'+
-	    ' <div class="modal-dialog">'+
-	    '<div class="row_between modal_title_wrap">'+
-	    ' <span>请选择支付方式</span>'+
-	    ' <img src="../images/login/login_close.png" class="close_style" onclick="closeDialg();" />'+
-	    '</div>'+
-	    ' <div class="row_between money_item">'+
-	    '<div>'+
-	    ' <span>应付金额:</span>'+
-	    '<span class="pay_money">￥'+money+'</span>'+
-	    '</div>'+
-	    '<span class="pay_warn">请在0小时30分内完成支付</span>'+
-	    ' </div>'+
-	    ' <form class="mui-input-group">'+
-	    '<div class="mui-input-row mui-radio mui-right radio_item payItem" style="height: 0.55rem!important;">'+
-	    ' <label>'+
-	    ' <div class="row">'+
-	    ' <span class="phone">月结</span>'+
-	    ' </div>'+
-	    '</label>'+
-	    ' <input name="radio1" type="radio" value="wxpay" checked>'+
-	    ' </div>'+
-	'</form>'+
-	'<<button type="button" class="mui-btn mui-btn-primary button" onclick="modalPay()">支付</button>'+
-	'<</div>'+
-	    '</div>')
-}else{
-	$("body").append('<div class="modal-mask col" id="payModal">'+
-	    '<div style="flex: 1;"></div>'+
-	    ' <div class="modal-dialog">'+
-	    '<div class="row_between modal_title_wrap">'+
-	    ' <span>请选择支付方式</span>'+
-	    ' <img src="../images/login/login_close.png" class="close_style" onclick="closeDialg();" />'+
-	    '</div>'+
-	    ' <div class="row_between money_item">'+
-	    '<div>'+
-	    ' <span>应付金额:</span>'+
-	    '<span class="pay_money">￥'+money+'</span>'+
-	    '</div>'+
-	    '<span class="pay_warn">请在0小时30分内完成支付</span>'+
-	    ' </div>'+
-	    ' <form class="mui-input-group">'+
-	    '<div class="mui-input-row mui-radio mui-right radio_item" style="height: 0.55rem!important;">'+
-	    ' <label>'+
-	    ' <div class="row">'+
-	    ' <img src="../images/payWay_03.jpg" alt="">'+
-	    ' <span class="phone">微信支付</span>'+
-	    ' </div>'+
-	    '</label>'+
-	    ' <input name="radio1" type="radio" value="wxpay" checked>'+
-	    ' </div>'+
-	    ' <div class="mui-input-row mui-radio mui-right radio_item" style="height: 0.55rem!important;">'+
-	    '<label>'+
-	    '<div class="row">'+
-	    ' <img src="../images/payWay_06.jpg" alt="">'+
-	    ' <span class="phone">支付宝支付</span>'+
-	   ' </div>'+
-	'</label>'+
-	'<input name="radio1" type="radio" value="alipay">'+
-	'</div>'+
-	'</form>'+
-	'<<button type="button" class="mui-btn mui-btn-primary button" onclick="modalPay()">支付</button>'+
-	'<</div>'+
-	    '</div>')
-}
+	if((typeof(monthPay)  != 'undefined') && monthPay){
+		$("body").append('<div class="modal-mask col" id="payModal">'+
+		    '<div style="flex: 1;"></div>'+
+		    ' <div class="modal-dialog">'+
+		    '<div class="row_between modal_title_wrap">'+
+		    ' <span>请选择支付方式</span>'+
+		    ' <img src="../images/login/login_close.png" class="close_style" onclick="closeDialg();" />'+
+		    '</div>'+
+		    ' <div class="row_between money_item">'+
+		    '<div>'+
+		    ' <span>应付金额:</span>'+
+		    '<span class="pay_money">￥'+money+'</span>'+
+		    '</div>'+
+		    '<span class="pay_warn">请在0小时30分内完成支付</span>'+
+		    ' </div>'+
+		    ' <form class="mui-input-group">'+
+		    '<div class="mui-input-row mui-radio mui-right radio_item payItem" style="height: 0.55rem!important;">'+
+		    ' <label>'+
+		    ' <div class="row">'+
+		    ' <span class="phone">月结</span>'+
+		    ' </div>'+
+		    '</label>'+
+		    ' <input name="radio1" type="radio" value="wxpay" checked>'+
+		    ' </div>'+
+		'</form>'+
+		'<<button type="button" class="mui-btn mui-btn-primary button" onclick="modalPay()">支付</button>'+
+		'<</div>'+
+		    '</div>')
+			
+		
+	}else{
+		$("body").append('<div class="modal-mask col" id="payModal">'+
+		    '<div style="flex: 1;"></div>'+
+		    ' <div class="modal-dialog">'+
+		    '<div class="row_between modal_title_wrap">'+
+		    ' <span>请选择支付方式</span>'+
+		    ' <img src="../images/login/login_close.png" class="close_style" onclick="closeDialg();" />'+
+		    '</div>'+
+		    ' <div class="row_between money_item">'+
+		    '<div>'+
+		    ' <span>应付金额:</span>'+
+		    '<span class="pay_money">￥'+money+'</span>'+
+		    '</div>'+
+		    '<span class="pay_warn">请在0小时30分内完成支付</span>'+
+		    ' </div>'+
+		    ' <form class="mui-input-group">'+
+		    '<div class="mui-input-row mui-radio mui-right radio_item" style="height: 0.55rem!important;">'+
+		    ' <label>'+
+		    ' <div class="row">'+
+		    ' <img src="../images/payWay_03.jpg" alt="">'+
+		    ' <span class="phone">微信支付</span>'+
+		    ' </div>'+
+		    '</label>'+
+		    ' <input name="radio1" type="radio" value="wxpay" checked>'+
+		    ' </div>'+
+		    ' <div class="mui-input-row mui-radio mui-right radio_item" style="height: 0.55rem!important;">'+
+		    '<label>'+
+		    '<div class="row">'+
+		    ' <img src="../images/payWay_06.jpg" alt="">'+
+		    ' <span class="phone">支付宝支付</span>'+
+		   ' </div>'+
+		'</label>'+
+		'<input name="radio1" type="radio" value="alipay">'+
+		'</div>'+
+		'</form>'+
+		'<<button type="button" class="mui-btn mui-btn-primary button" onclick="modalPay()">支付</button>'+
+		'<</div>'+
+		    '</div>')
+	}
 	
 
 }
 
 function modalPay()
 {
-	if(monthPay){
+	if((typeof(monthPay)  != 'undefined') && monthPay){
 		//企业月结
 		pay(3);
+		
+		
 	}else{
 		pay($('input:radio[name="radio1"]:checked').val());
 	}
@@ -105,7 +109,9 @@ function pay(payWay) {
 		//企业月结算
 		PAYSERVER = 3
 	}
-	if(monthPay != 1){
+	if((typeof(monthPay)  != 'undefined') && monthPay){
+		
+	}else{
 		console.log("payWay==>"+payWay);
 		console.log("payserver==>"+PAYSERVER);
 		for (var i in channels)
@@ -140,7 +146,7 @@ function pay(payWay) {
         console.log(JSON.stringify(channel));
         console.log(JSON.stringify(data));
 		
-		if(monthPay){
+		if((typeof(monthPay)  != 'undefined') && (monthPay == 1)){
 			plus.nativeUI.alert("支付成功！", function() {
 				
 				var h = plus.webview.getWebviewById("my_order.html");
